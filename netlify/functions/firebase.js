0be48fa7088e156a74788d20833b128f13c53bcd
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-05-16 15:29:41 -0500

    1

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyA77-4YtlYcf6l0llKH7uIwLDoOjk7Y660",
+  authDomain: "kiei-451-f3a4a.firebaseapp.com",
+  projectId: "kiei-451-f3a4a",
+  storageBucket: "kiei-451-f3a4a.appspot.com",
+  messagingSenderId: "1058781797707",
+  appId: "1:1058781797707:web:682266636687c031265cce"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
